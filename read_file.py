3af5bdc4b10d6fb5824a767536f02a16f3b36e35
SrinathAkkem <77010468+SrinathAkkem@users.noreply.github.com>
2021-07-19 19:10:42 +0530

    Create read_file.py

---
+++ read_file.py
@@ -0,0 +1,7 @@
+def read_file(file_name):
+    opened_file = open(file_name, 'r')
+    lines_list = []
+    for line in opened_file:
+        line = line.split()
+        lines_list.append(line)
+    return lines_list
